fd914ff4b85575de0a6ed6334a00d8c3802a4fec
ANWDM <pv4003337@gmail.com>
2023-05-01 02:59:17 +0530

    added some feature

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -1,3 +1,3 @@
-export const Api = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=AIzaSyB0AHrkRB54Lv3ISPzwOYCXxVWYkjs48-8";
+export const Api = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${import.meta.env.VITE_API_KEY}`;
 
 export const  suggestionApi = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
