b290153203c4c7f0dc678cef88f9431959a39c82
ANWDM <pv4003337@gmail.com>
2023-06-16 01:37:17 +0530

    making chnges in ui

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -61,20 +61,22 @@ const Nav = () => {
              
             </div>
           
-            <div className="w-full sm:w-1/2 mt-2 relative sm:mt-0">
+            <div className="w-full sm:w-1/2 mt-2 flex items-center relative sm:mt-0">
               <input
-                className="border-2 border-gray-600 w-full sm:w-2/4 px-2 py-1"
+                type="text"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700
+               dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required
                 name="search"
                 placeholder="Search"
-                type="text"
                 value={search}
                 onFocus={() => setToggleSearch(true)}
                 onBlur={() => setToggleSearch(false)}
                 onChange={(e) => setSearch(e.target.value) }
               />
-              <button className="px-4 py-1 mt-2 sm:mt-0 sm:ml-2 bg-blue-500 text-white">
-                Search
-              </button>
+              <button type="submit" class="p-2.5 ml-2 text-sm font-medium text-white bg-red-600 rounded-lg borde hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+               <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                <span class="sr-only">Search</span>
+           </button>
             </div>
             {toggleSearch && <div className="bg-white w-96 top-14 fixed left-[24rem] rounded-md">
                <ul className="text-lg space-y-2 space-x-2 🔍" >
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -5,27 +5,145 @@ const SideBar = () => {
     const isMenu = useSelector(store => store.app.isSideBar)
 
     return !isMenu ? null : (
-        <div className="flex flex-col h-full fixed text-gray-900">
-        <ul className="p-4">
-          <Link to='/'><li className="py-2 hover:text-gray-200">Home</li></Link>
-          <li className="py-2 hover:text-gray-200">Shorts</li>
-          <li className="py-2 hover:text-gray-200">Subscriptions</li>
-          <li className="py-2 hover:text-gray-200">Library</li>
-          <li className="py-2 hover:text-gray-200">History</li>
-        </ul>
-        <ul className="p-4 ">
-          <li className="py-2 hover:text-gray-200">Explore</li>
-          <li className="py-2 hover:text-gray-200">Trending</li>
-          <li className="py-2 hover:text-gray-200">Shopping</li>
-          <li className="py-2 hover:text-gray-200">Music</li>
-          <li className="py-2 hover:text-gray-200">Movies & Shows</li>
-          <li className="py-2 hover:text-gray-200">Gaming</li>
-          <li className="py-2 hover:text-gray-200">News</li>
-          <li className="py-2 hover:text-gray-200">Sports</li>
-          <li className="py-2 hover:text-gray-200">Learning</li>
-          <li className="py-2 hover:text-gray-200">Fashion & Beauty</li>
-        </ul>
+  <div class="flex flex-row">
+      <div className=".sidebar" >
+          <div class="flex flex-col w-full mb-2">
+              <div class="flex items-center space-x-5 bg-sideBarHoverBg px-7 py-2.5 cursor-pointer">
+
+                  <h2 class="text-sm font-semibold text-gray-600">Home</h2>
+              </div>
+
+              <div class="flex items-center space-x-5 hover:bg-sideBarHoverBg px-7 py-2.5 cursor-pointer">
+                  <h2 class="text-sm text-gray-600">Explore</h2>
+              </div>
+
+              <div class="flex items-center space-x-5 hover:bg-sideBarHoverBg px-7 py-2.5 cursor-pointer">
+                  <h2 class="text-sm text-gray-600">Subscriptions</h2>
+              </div>
+
+          </div>
+
+          <div class="border border-searchIconBg"></div>
+          <div class="flex flex-col w-full mb-2 mt-3">
+
+              <div class="flex items-center space-x-5 hover:bg-sideBarHoverBg px-7 py-2.5 cursor-pointer">
+                  <h2 class="text-sm text-gray-600">Library</h2>
+              </div>
+
+              <div class="flex items-center space-x-5 hover:bg-sideBarHoverBg px-7 py-2.5 cursor-pointer">
+                  <h2 class="text-sm text-gray-600">History</h2>
+              </div>
+
+              <div class="flex items-center space-x-5 hover:bg-sideBarHoverBg px-7 py-2.5 cursor-pointer">
+                
+                  <h2 class="text-sm text-gray-600">Your videos</h2>
+              </div>
+
+              <div class="flex items-center space-x-5 hover:bg-sideBarHoverBg px-7 py-2.5 cursor-pointer">
+                  <h2 class="text-sm text-gray-600">Watch later</h2>
+              </div>
+
+              <div class="flex items-center space-x-5 hover:bg-sideBarHoverBg px-7 py-2.5 cursor-pointer">
+                  <h2 class="text-sm text-gray-600">Liked videos</h2>
+              </div>
+
+          </div>
+
+          <div class="border border-searchIconBg"></div>
+          <div class="flex flex-col w-full mb-2 mt-3">
+              <h2 class="uppercase text-sm px-7 py-1.5 text-gray-400">more from youtube</h2>
+
+              <div class="flex items-center space-x-5 hover:bg-sideBarHoverBg px-7 py-2.5 cursor-pointer">
+
+                  <h2 class="text-sm text-gray-600">YouTube Premium</h2>
+              </div>
+
+              <div class="flex items-center space-x-5 hover:bg-sideBarHoverBg px-7 py-2.5 cursor-pointer">
+                  
+                  <h2 class="text-sm text-gray-600">Films</h2>
+              </div>
+
+              <div class="flex items-center space-x-5 hover:bg-sideBarHoverBg px-7 py-2.5 cursor-pointer">
+                  
+                  <h2 class="text-sm text-gray-600">Gaming</h2>
+              </div>
+
+              <div class="flex items-center space-x-5 hover:bg-sideBarHoverBg px-7 py-2.5 cursor-pointer">
+                  
+                  <h2 class="text-sm text-gray-600">Live</h2>
+              </div>
+
+              <div class="flex items-center space-x-5 hover:bg-sideBarHoverBg px-7 py-2.5 cursor-pointer">
+                  
+                  <h2 class="text-sm text-gray-600">Fashion & beauty</h2>
+              </div>
+
+              <div class="flex items-center space-x-5 hover:bg-sideBarHoverBg px-7 py-2.5 cursor-pointer">
+                  
+                  <h2 class="text-sm text-gray-600">Learning</h2>
+              </div>
+
+              <div class="flex items-center space-x-5 hover:bg-sideBarHoverBg px-7 py-2.5 cursor-pointer">
+                  
+                  <h2 class="text-sm text-gray-600">Sport</h2>
+              </div>
+
+          </div>
+
+          <div class="border border-searchIconBg"></div>
+
+          <div class="flex flex-col w-full mb-2 mt-3">
+
+              <div class="flex items-center space-x-5 hover:bg-sideBarHoverBg px-7 py-2.5 cursor-pointer">
+                  
+                  <h2 class="text-sm text-gray-600">Settings</h2>
+              </div>
+
+              <div class="flex items-center space-x-5 hover:bg-sideBarHoverBg px-7 py-2.5 cursor-pointer">
+                  
+                  <h2 class="text-sm text-gray-600">Report history</h2>
+              </div>
+
+              <div class="flex items-center space-x-5 hover:bg-sideBarHoverBg px-7 py-2.5 cursor-pointer">
+                  
+                  <h2 class="text-sm text-gray-600">Help</h2>
+              </div>
+
+              <div class="flex items-center space-x-5 hover:bg-sideBarHoverBg px-7 py-2.5 cursor-pointer">
+                  
+                  <h2 class="text-sm text-gray-600">Send feedback</h2>
+              </div>
+          </div>
+
+          <div class="border border-searchIconBg"></div>
+
+          <div class="flex flex-col px-6 py-2">
+              <div class="flex flex-wrap space-x-2">
+                  <p class="text-xs text-gray-400" href="">About</p>
+                  <p class="text-xs text-gray-400" href="">Press</p>
+                  <p class="text-xs text-gray-400" href="">Copyright</p>
+                  </div>
+                  <div class="flex flex-wrap space-x-2">
+                  <p class="text-xs text-gray-400" href="">Contact us</p>
+                  <p class="text-xs text-gray-400" href="">Creator</p>
+                  <p class="text-xs text-gray-400" href="">Advertise</p>
+                  </div>
+                  <p class="text-xs text-gray-400" href="">Developers</p>
+          </div>
+          <div class="flex flex-col px-6 py-2">
+              <div class="flex flex-wrap space-x-2">
+                  <p class="text-xs text-gray-400" href="">Terms</p>
+                  <p class="text-xs text-gray-400" href="">Privacy</p>
+                  <p class="text-xs text-gray-400" href="">Policy & Safety</p>
+                  </div>
+                  <p class="text-xs text-gray-400" href="">How YouTube works</p>
+                  <p class="text-xs text-gray-400" href="">Test new features</p>
+          </div>
+            <span class="text-gray-500 text-xs px-6 py-2">&copy; 2021 Google LLC</span>
+
       </div>
+  </div>
+
   )
 }
 
