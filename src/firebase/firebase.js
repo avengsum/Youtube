7cb63235d3227ab3b63ae020405468a88aa32db2
ANWDM <pv4003337@gmail.com>
2023-06-21 18:07:16 +0530

    add video implemented

---
+++ src/firebase/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBEqJc5Yrzl8b_bRNYuLYFx1rWScVCQeUo",
+  authDomain: "clone-ac804.firebaseapp.com",
+  projectId: "clone-ac804",
+  storageBucket: "clone-ac804.appspot.com",
+  messagingSenderId: "563417005799",
+  appId: "1:563417005799:web:a3f50768ac473488fdac42"
+};
+
+const app = initializeApp(firebaseConfig);
