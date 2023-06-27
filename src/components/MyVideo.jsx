5424f58739502f78cb3dc1f7bd7085f41eac42b7
ANWDM <pv4003337@gmail.com>
2023-06-27 23:31:30 +0530

    added

--- src/components/MyVideo.jsx
+++ src/components/MyVideo.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import im from '../assets/im.jpg'
-import UploadVideo from './UploadVideo'
+import { database } from '../firebase/firebase'
+import { collection, getDocs } from 'firebase/firestore'
 
 const MyVideo = () => {
+
+    const [uploadData,setUploadData] = useState([])
+
+    const getData = async () => {
+        const Data = await getDocs(collection(database,'video-data'))
+        Data.forEach((doc) => {
+            console.log(doc.id , '=>', doc.data());
+            setUploadData(doc.data())
+        })
+
+    }
+    useEffect(() => {
+        getData()
+    },[])
+
   return (
     <div className='max-w-2xl mx-auto p-4'>
         <table className='min-w-full'>
@@ -21,14 +37,13 @@ const MyVideo = () => {
 
                 </td>
                 <td className='px-6 py-4 w-5/12 text-sm font-medium text-gray-900 relative'>
-                    <h1 className='text-black text-lg font-bold mb-4'>vuugggugug</h1>
+                    <h1 className='text-black text-lg font-bold mb-4'>{uploadData.title}</h1>
                     <div className='w-full'>
                     <button
                     className='text-white absolute bottom-4 right-4 text-xs bg-red-600 hover:bg-red-700
                      font-bold py-1 px-1 float-right rounded cursor-pointer' 
                     >Delete</button>
-                    </div>
-                    
+                    </div>    
                 </td>
             </tr>
             
